c1174ba36a3f5dadc1251ed4080d9fd6f85c3eab
nopparat9pi <nopparat.mueangkaew@gmail.com>
2017-08-30 14:13:48 +0700

    Fix add file all

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBOP5UywuOjE97XzLRBLG6Q4J3iHayZnqY",
+  authDomain: "document-api-v2.firebaseapp.com",
+  databaseURL: "https://document-api-v2.firebaseio.com",
+  projectId: "document-api-v2",
+  storageBucket: "document-api-v2.appspot.com",
+  messagingSenderId: "489404796809"
+};
