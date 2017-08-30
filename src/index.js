597aa133acaa3300356c8d3cf92f2ffeabc3ef62
nopparat9pi <nopparat.mueangkaew@gmail.com>
2017-08-30 14:02:12 +0700

    use redux with https://github.com/r-park/todo-react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
+import './views/styles/styles.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { initAuth } from './auth';
+import history from './history';
+import configureStore from './store';
+import registerServiceWorker from './utils/register-service-worker';
+import App from './views/app';
+
+
+const store = configureStore();
+const rootElement = document.getElementById('root');
+
+
+function render(Component) {
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div>
+          <Component/>
+        </div>
+      </ConnectedRouter>
+    </Provider>,
+    rootElement
+  );
+}
+
+
+if (module.hot) {
+  module.hot.accept('./views/app', () => {
+    render(require('./views/app').default);
+  })
+}
+
+
 registerServiceWorker();
+
+
+initAuth(store.dispatch)
+  .then(() => render(App))
+  .catch(error => console.error(error));
